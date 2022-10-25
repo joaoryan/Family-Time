ae9b9ec5035b9f1364ec2125abc3d17bd56d3e41
joaoryan <sjoaoryan@gmail.com>
2022-10-25 12:34:49 -0300

    chore: code

--- code/main.py
+++ code/main.py
@@ -4,83 +4,54 @@ from db.database import Graph
 
 class PersonDAO(object):
     def __init__(self):
-        self.db = Graph(uri='bolt://52.91.64.64:7687',
-                        user='neo4j', password='board-apparatuses-smashes')
+        self.db = Graph(uri='bolt://35.172.234.92:7687',
+                        user='neo4j', password='machinery-yell-bangs')
 
-    def create(self, person):
-        return self.db.execute_query('CREATE (n:Person {name:$name, age:$age}) return n',
-                                     {'name': person['name'], 'age': person['age']})
+    def read_by_father(self, nome):
+        return self.db.execute_query('MATCH (n:Pai {nome:$nome}) -- (f:Filho) RETURN f',
+                                     {'nome': nome})
 
-    def read_by_name(self, person):
-        return self.db.execute_query('MATCH (n:Person {name:$name}) RETURN n',
-                                     {'name': person['name']})
-    
-    def read_all_nodes(self):
-        return self.db.execute_query('MATCH (n) RETURN n')
+    def read_by_engineer(self):
+        return self.db.execute_query('MATCH (n:Engenheiro) RETURN n')
 
-    def update_age(self, person):
-        return self.db.execute_query('MATCH (n:Person {name:$name}) SET n.age = $age RETURN n',
-                                     {'name': person['name'], 'age': person['age']})
+    def read_by_boyfriend(self, nome):
+        return self.db.execute_query('MATCH (f:Filho {nome:$nome}) <-[n:NAMORADO_DE]- (c:Cunhado) RETURN n, c',
+                                     {'nome': nome})
 
-    def delete(self, person):
-        return self.db.execute_query('MATCH (n:Person {name:$name}) DELETE n',
-                                     {'name': person['name']})
-
-    def delete_all_nodes(self):
-        return self.db.execute_query('MATCH (n) DETACH DELETE n')
-
-    def create_relation(self, person1, person2, year):
-        return self.db.execute_query('MATCH (n:Person {name:$name1}), (m:Person {name:$name2}) CREATE (n)-[r:KNOWS{year: $year}]->(m) RETURN n, r, m',
-                                     {'name1': person1['name'], 'name2': person2['name'], 'year': year})
-
-    def read_relation(self, person1, person2):
-        return self.db.execute_query('MATCH (n:Person {name:$name1})-[r]->(m:Person {name:$name2}) RETURN n, r, m',
-                                     {'name1': person1['name'], 'name2': person2['name']})
 
 def divider():
     print('\n' + '-' * 80 + '\n')
 
+
 dao = PersonDAO()
 
-while 1:    
-    option = input('1. Create\n2. Read\n3. Update\n4. Delete\n')
+pp('Quem da família é Engenheiro?')
+engineer = dao.read_by_engineer()
+if len(engineer) == 0:
+    pp('Não possui nenhum engenheiro na familia, ainda')
+else:
+    pp(engineer)
 
-    if option == '1':
-        name = input('  Name: ')
-        age = input('   Age: ')
-        person = {
-            'name': name,
-            'age': age
-        }
-        aux = dao.create(person)
-        divider()
+divider()
 
-    elif option == '2':
-        aux = dao.read_all_nodes()
-        pp(aux)
-        divider()
+pp('Fulano de tal é pai de quem?')
+pai = input('Digite o nome do pai:')
+sons = dao.read_by_father(pai)
+if len(sons) == 0:
+    pp('Este pai não possui filhos')
+else:
+    pp(sons)
 
-    elif option == '3':
-        name = input('  Name: ')
-        age = input('   Age: ')
-        person = {
-            'name': name,
-            'age': age
-        }
-        
-        aux = dao.update_age(person)
-        divider()
+divider()
 
-    elif option == '4':
-        name = input('  Name: ')
-        person = {
-            'name': name
-        }
-        
-        aux = dao.delete(person)
-        divider()
+pp('Sicrana de tal namora com quem desde quando?')
+girl = input('Digite o nome da sicrana:')
+boy = dao.read_by_boyfriend(girl)
+if len(boy) == 0:
+    pp('Não possui nenhum namorado')
+else:
+    pp(boy)
 
-    else:
-        break
+divider()
 
 dao.db.close()
